271d5f327602374964e6f00b9b49b8debd1c1f2e
Esteban Martin <estebanmartin82@gmail.com>
2024-06-03 13:51:32 -0300

    modificando 2da entrega

--- app/index.js
+++ app/index.js
@@ -1,17 +1,13 @@
 // console.log(document.body);
 
-let formulario = document.querySelector('form'); // Cambiado para seleccionar el formulario en lugar de todos los inputs
+let formulario = document.querySelector('form'); 
 
-// Imprime el formulario en la consola
+
 console.log(formulario);
 
-// Añade un event listener al formulario para el evento submit
 formulario.addEventListener('submit', function(e) {
-    e.preventDefault(); // Previene el envío del formulario
+    e.preventDefault(); // 
     
-    // Selecciona el primer input dentro del formulario y obtiene su valor
     let mail = formulario.querySelector('input').value;
-    
-    // Imprime el valor del input en la consola
     console.log(mail);
 });
